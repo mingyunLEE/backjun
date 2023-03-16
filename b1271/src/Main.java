7a3c88fa09b615bedca246a6b4fbdb86896ac0f7
mingyunLEE <leemingyun@naver.com>
2023-03-16 12:49:08 +0900

    긴자리 계산

--- b1271/src/Main.java
+++ b1271/src/Main.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        BigInteger n1 = sc.nextBigInteger();
+        BigInteger n2 = sc.nextBigInteger();
+
+        System.out.println(n1.add(n2));
+        System.out.println(n1.subtract(n2));
+        System.out.println(n1.multiply(n2));
+
     }
 }
