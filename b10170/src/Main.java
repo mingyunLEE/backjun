3fcac57a44a99718f88a766c26e2e5fe8863eaa7
mingyunLEE <leemingyun@naver.com>
2023-03-17 15:16:47 +0900

    NFC West vs North

---
+++ b10170/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("NFC West       W   L  T\n");
+        System.out.printf("-----------------------\n");
+        System.out.printf("Seattle        13  3  0\n");
+        System.out.printf("San Francisco  12  4  0\n");
+        System.out.printf("Arizona        10  6  0\n");
+        System.out.printf("St. Louis      7   9  0\n");
+        System.out.println("");
+        System.out.printf("NFC North      W   L  T\n");
+        System.out.printf("-----------------------\n");
+        System.out.printf("Green Bay      8   7  1\n");
+        System.out.printf("Chicago        8   8  0\n");
+        System.out.printf("Detroit        7   9  0\n");
+        System.out.printf("Minnesota      5  10  1\n");
+    }
+}
