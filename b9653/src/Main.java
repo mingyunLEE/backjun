8f6e7ce9cdbc6e9d370eae0eec32610e4717e407
mingyunLEE <leemingyun@naver.com>
2023-03-17 15:06:44 +0900

    스타워즈 로고

---
+++ b9653/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("    8888888888  888    88888\n");
+        System.out.printf("   88     88   88 88   88  88\n");
+        System.out.printf("    8888  88  88   88  88888\n");
+        System.out.printf("       88 88 888888888 88   88\n");
+        System.out.printf("88888888  88 88     88 88    888888\n");
+        System.out.printf("\n");
+        System.out.printf("88  88  88   888    88888    888888\n");
+        System.out.printf("88  88  88  88 88   88  88  88\n");
+        System.out.printf("88 8888 88 88   88  88888    8888\n");
+        System.out.printf(" 888  888 888888888 88  88      88\n");
+        System.out.printf("  88  88  88     88 88   88888888\n");
+    }
+}
