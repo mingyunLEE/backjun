e93b30a859c9f406571f277bd9c086ab5456a0e3
mingyunLEE <leemingyun@naver.com>
2023-03-17 14:10:47 +0900

    콜센터

---
+++ b5339/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("     /~\\\n");
+        System.out.printf("    ( oo|\n");
+        System.out.printf("    _\\=/_\n");
+        System.out.printf("   /  _  \\\n");
+        System.out.printf("  //|/.\\|\\\\\n");
+        System.out.printf(" ||  \\ /  ||\n");
+        System.out.printf("============\n");
+        System.out.printf("|          |\n");
+        System.out.printf("|          |\n");
+        System.out.printf("|          |\n");
+    }
+}
+
