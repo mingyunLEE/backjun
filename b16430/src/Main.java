0879410333254f01cfa3e7f76d3457975a5df217
mingyunLEE <leemingyun@naver.com>
2023-03-17 20:43:25 +0900

    제리와 톰

---
+++ b16430/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
+        System.out.println(number2-number1+" "+ number2);
+
+    }
+}
