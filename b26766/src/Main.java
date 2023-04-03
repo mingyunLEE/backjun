ea3aa3ff5c277df3863f152f28f5222a8f37c686
mingyunLEE <leemingyun@naver.com>
2023-04-03 10:06:04 +0900

    Serca

---
+++ b26766/src/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num; i++) {
+            System.out.printf(" @@@   @@@ \n");
+            System.out.printf("@   @ @   @\n");
+            System.out.printf("@    @    @\n");
+            System.out.printf("@         @\n");
+            System.out.printf(" @       @ \n");
+            System.out.printf("  @     @  \n");
+            System.out.printf("   @   @   \n");
+            System.out.printf("    @ @    \n");
+            System.out.printf("     @     \n");
+        }
+    }
+}
