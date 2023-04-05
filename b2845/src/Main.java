530634ca632a251614411d846b58599e325170aa
mingyunLEE <leemingyun@naver.com>
2023-04-05 11:42:32 +0900

    파티가 끝나고 난 뒤

---
+++ b2845/src/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int space = Integer.parseInt(st.nextToken());
+        int people = Integer.parseInt(st.nextToken());
+        int sp = space * people;
+
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while(st.hasMoreTokens()){
+            int n = Integer.parseInt(st.nextToken());
+            sb.append(n - sp + "");
+        }
+        System.out.println(sb);
+    }
+}
